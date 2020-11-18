40559d5e9edef6875fbb07b45cb2bdfc8900def1
haweecodes <haweesajib@yahoo.com>
2020-11-18 15:51:38 +0600

    search api implemented

---
+++ src/components/SearchComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-text-field v-model="searchText" placeholder="Search your restaurant"
+    outlined @keyup.enter="searchRestaurant"></v-text-field>
+</template>
+
+<script>
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+// Define the component in class-style
+@Component
+export default class SearchComponent extends Vue {
+  searchText = '';
+
+  // Methods will be component methods
+  searchRestaurant() {
+    this.$store.dispatch('searchRestaurant', this.searchText);
+    console.log(process.env);
+    // search api
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://some-domain.com/api/',
+  timeout: 1000,
+  headers: {
+    'user-key': process.env.VUE_APP_API_KEY,
+  },
+});
 
 Vue.use(Vuex);
 
@@ -9,6 +18,20 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
+    searchRestaurant(context, payload) {
+      instance.get(`https://developers.zomato.com/api/v2.1/search?q=${payload}`, {})
+        .then((response: any) => {
+        // handle success
+          console.log(response);
+        })
+        .catch((error: any) => {
+        // handle error
+          console.log(error);
+        })
+        .then(() => {
+        // always executed
+        });
+    },
   },
   modules: {
   },
